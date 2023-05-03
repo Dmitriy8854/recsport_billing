77f17f8d705bb19fe8cf281363037ab1a41f4f5d
Морозов Дмитрий <dmit.morozoff2009@yandex.ru>
2023-05-03 14:22:09 +0300

    Templates and views

--- billing/acquiring/views.py
+++ billing/acquiring/views.py
@@ -15,7 +15,10 @@ from .models import Product
   #  product = get_object_or_404(Product, id=id, slug=slug, available=True)
  #   cart_product_form = CartAddProductForm()
   #  return render(request, 'acquiring/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
-def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, available=True)
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
-    return render(request, 'acquiring/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
+    return render(request, 'shop/product/detail.html',
+        {'product': product,
+        'cart_product_form': cart_product_form})
